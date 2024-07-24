1e529b88c71aa17e6ea38a6a2b73075cc2e37d3f
ndubovskov <ndubovskov@DESKTOP-GVJB0RR>
2024-07-24 17:18:43 +0500

    Удалил ссылку на SignCutter

--- Usings.cs
+++
@@ -1,2 +0,0 @@
-﻿global using SignCutter;
-global using System.Linq;
---
+++ Xml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace FormSorter;
+
+public class Xml
+{
+    public void CutTag(string file, string newFile, string tag)
+    {
+        XDocument doc = XDocument.Load(file);
+        XElement nodeToRemove = doc.Descendants().FirstOrDefault(e => e.Name.LocalName == tag);
+        nodeToRemove?.Remove();
+        using (var writer = new StreamWriter(newFile.ToUpper(), false, new UTF8Encoding(false)))
+        {
+            doc.Save(writer);
+        }
+    }
+
+    public string GetUmmsId(string file)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(file);
+        var result = string.Empty;
+
+        var node = xmlDoc.GetElementsByTagName("ummsId");
+
+        if (node?.Count > 0)
+        {
+            result = node[0].InnerText;
+        }
+
+        return result;
+    }
+
+    public static string GetHotelId(string xmlFile)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        XmlNodeList node;
+        xmlDoc.Load(xmlFile);
+
+        node = xmlDoc.GetElementsByTagName("supplierInfo");
+        if (node.Count == 0)
+        {
+            node = xmlDoc.GetElementsByTagName("ns1:supplierInfo");
+        }
+
+        string hotelId = null;
+
+        if (node?.Count > 0)
+        {
+            hotelId = node[0].InnerText;
+        }
+
+        return hotelId;
+    }
+}
